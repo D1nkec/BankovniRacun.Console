25d4e6ea433d1bccddddc596c7cb3b7136d61acd
Dino <Dino@DESKTOP-CIN309U>
2024-07-05 14:37:29 +0200

    Add project files.

---
+++ BankovniRacun.Console/BankovniRacun.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class BankovniRacun
+    {
+       
+        public decimal StanjeRacuna { get; private set; }
+        public List<string> Transakcije { get; set; }
+
+        public BankovniRacun(decimal pocetnoStanje)
+        {
+            StanjeRacuna = pocetnoStanje;
+            Transakcije = new List<string>() { $"Početno stanje: {pocetnoStanje}" };
+        }
+
+
+        public  void Uplata(decimal iznos)
+        {
+          StanjeRacuna += iznos;
+          Transakcije.Add($"Uplata: {iznos}");
+        }
+
+
+
+        public void IspisiTransakcije()
+        {
+            foreach (var t in Transakcije)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+
---
+++ BankovniRacun.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+  
+             var racun = new BankovniRacun(2000m);
+
+              Console.WriteLine($"Početno stanje računa: {racun.StanjeRacuna}");
+
+        racun.Uplata(200m);
+        racun.Uplata(100m);
+        racun.Uplata(600m);
+        racun.Uplata(300m);
+        racun.Uplata(200m);
+
+        racun.IspisiTransakcije();
+
+        Console.WriteLine($"Završno stanje računa: {racun.StanjeRacuna:C}");
+    }
+    }
+
